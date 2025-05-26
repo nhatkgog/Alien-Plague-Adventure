cc4e0fd6afcc4daa256840dd850a964ee65dd399
NhiDAG <amijeon2019@gmail.com>
2025-05-26 23:39:31 +0700

    feature/build-new-main-menu

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip hoverClip;
+    public AudioClip clickSound;
+    private AudioSource audioSource;
+    private Button button;
+
+    void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+        button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (hoverClip != null)
+        {
+            audioSource.PlayOneShot(hoverClip);
+        }
+    }
+
+    private void PlayClickSound()
+    {
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    public static MainMenuMusic instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); 
+
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && !audioSource.isPlaying)
+            {
+                audioSource.loop = true;
+                audioSource.playOnAwake = false;
+                audioSource.Play(); 
+            }
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
