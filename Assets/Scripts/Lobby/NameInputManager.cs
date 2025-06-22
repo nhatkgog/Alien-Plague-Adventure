2c031c304d45c8067df2dee4c8ce8bfbd1eed31a
Nhi Dang <nhidy.it@gmail.com>
2025-06-22 18:53:04 +0700

    save player prefab

---
+++ Assets/Scripts/Lobby/NameInputManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enterNameCanvas;
+    [SerializeField] private GameObject lobbyCanvas;
+
+    [SerializeField] private TMP_InputField nameInputField; 
+    [SerializeField] private TMP_Text warningText;
+    [SerializeField] private TMP_Text playerName;
+    private Coroutine warningCoroutine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        warningText.text = "";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            OnClickOK();
+        }
+    }
+
+    public void OnClickOK()
+    {
+        SubmitName();
+    }
+
+    void SubmitName()
+    {
+        string yourName = nameInputField.text;
+        warningText.text = "";
+        if (!string.IsNullOrEmpty(yourName))
+        {
+            playerName.text = yourName;
+            PlayerPrefs.SetString("PlayerName", yourName);
+            PlayerPrefs.Save();
+
+            enterNameCanvas.SetActive(false);
+            lobbyCanvas.SetActive(true);
+        }
+        else
+        {
+            if (warningCoroutine != null)
+            {
+                StopCoroutine(warningCoroutine);
+            }
+
+            warningCoroutine = StartCoroutine(ShowWarningTemporarily("Please enter your name!"));
+        }
+    }
+
+    IEnumerator ShowWarningTemporarily(string message)
+    {
+        warningText.text = message;
+        yield return new WaitForSeconds(10);
+        warningText.text = "";
+    }
+}
